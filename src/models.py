af353a50139dc31d5bedf33d562bf3da55b60728
George Mihov <gmihov001@gmail.com>
2020-08-18 03:28:09 +0000

    Updates

--- src/models.py
+++ src/models.py
@@ -14,6 +14,21 @@ class Person(Base):
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    username = Column(String(250), nullable=False)
+
+class Image(Base):
+    __tablename__ = 'image'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    url = Column(String(250), nullable=False)
+    size = Column(String(250), nullable=False)
+
+class Post(Base):
+    __tablename__ = 'post'
+    id = Column(Integer, primary_key=True)
+    date = Column(String(250), nullable=False)
+    image = relationship(Image)
 
 class Address(Base):
     __tablename__ = 'address'
